af94eb30ad39ef69f450a1958515f8654e330634
Shaheem <Shaheemsalie349@gmail.com>
2026-06-11 11:24:26 +0200

    addedfiles

---
+++ MyPortfolio/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <navbar/>
+  <router-view/>
+  <footers/>
+
+</template>
+
+<script>
+// import navbar from './components/Navbar.vue'
+// export default {
+//   components: {
+//     navbar
+//   }
+
+
+// }
+
+</script>
+
+<style>
+/* #app {
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+   overflow: hidden;
+   scroll-behavior: smooth;
+} */
+</style>

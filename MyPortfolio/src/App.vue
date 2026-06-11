85aa2f64a4e53ca98dc8b734d1a2e556bbd94962
Shaheem <Shaheemsalie349@gmail.com>
2026-06-11 13:26:15 +0200

    minutes

--- MyPortfolio/src/App.vue
+++ MyPortfolio/src/App.vue
@@ -6,14 +6,14 @@
 </template>
 
 <script>
-// import navbar from './components/Navbar.vue'
-// export default {
-//   components: {
-//     navbar
-//   }
+import navbar from './components/Navbar.vue'
+export default {
+  components: {
+    navbar
+  }
 
 
-// }
+}
 
 </script>
 
--- MyPortfolio/src/views/HomeView.vue
+++ MyPortfolio/src/views/HomeView.vue
@@ -1,5 +1,70 @@
 <template>
-	  <div class="home">
-	<h1>This is a home page</h1>
+  <div class="home">
+	
   </div>
+ 
+  <div class="Gallary">
+   
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-14">
+    <div class="grid gap-4">
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
+        </div>
+        <div>
+            <img class="bozo h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+  </div> 
 </template>
+
+
+<style>
+ .bozo{
+	 /* border: solid 2px red; */
+	 transition: 0.5s ease-in-out;
+ }
+
+ .bozo:hover{
+	transition: 0.5s;
+    transform: scale(1.1	);
+ }
+</style>
